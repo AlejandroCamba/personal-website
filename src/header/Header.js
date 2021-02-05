0f05a8d79cf82e6002f80f3df577c8e097977af6
alejandro camba <alejandrocamba@MacBook-Pro-de-alejandro.local>
2021-02-05 11:04:33 -0400

    updating contact

--- src/header/Header.js
+++ src/header/Header.js
@@ -28,8 +28,8 @@ export class Header extends Component {
                 >
                     <h1 className="title" style={this.props.didStyleChanged ? this.downloadableStyle.heading : null}><b className="name">{this.props.content.name}</b> {this.props.content.title}</h1>
                     <div className="contact-info" style={this.props.didStyleChanged ? this.downloadableStyle.contact : null}>
-                      <span className="contact-content"><b>Address:</b> Warschauer St. 1234 Berlin, 8855</span>
-                      <label className="contact-content"><b>Phone:</b> +49 333 5666 7733</label>
+                      <span className="contact-content"><b>Address:</b> Caracas, Venezuela</span>
+                      <label className="contact-content"><b>Phone:</b> +58 424 8498826</label>
                       <label className="contact-content"><b>Email:</b> alejandroantoniocamb@gmail.com</label>
                       <label className="contact-content"><b>Date of Birth:</b> 14/10/1994 (dd/mm/yyyy)</label>
                       <label className="contact-content"><b>Civil Status:</b> Single</label>
